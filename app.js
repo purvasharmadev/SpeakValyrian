785f7c7f911ede88189a33b3144207b161caae15
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-12-10 12:27:06 +0530

    Feat: Add Translation functions

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#translate");
+var txt = document.querySelector("#inputTxt");
+var outputDiv = document.querySelector("#output")
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+function getTranslateURL(text){
+    return serverURL + "?" +"text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error Occured",error)
+    alert("something wrong with server! Try again after sometime :( ")
+}
+
+function clickHandler(){
+    
+    console.log("button is clicked");
+    var inputTxt = txt.value
+    fetch(getTranslateURL(inputTxt))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        console.log(json.contents.translated)
+    } )
+    .catch(errorHandler)
+
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
